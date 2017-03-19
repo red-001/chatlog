d8bcad712a08fcbfcda8940760738f22cd07e907
red-001 <red-001@outlook.ie>
2017-03-19 17:55:00 +0000

    Add timestamp.

--- init.lua
+++ init.lua
@@ -1,9 +1,18 @@
 local modstorage = core.get_mod_storage()
+if modstorage:get_string("setup") == "" then
+	modstorage:set_string("timestamp","%H:%M")
+	modstorage:set_string("setup","finished")
+end
 
 local function log_message(message)
 	local id = modstorage:get_int("chat_message_id")
 	print("Debug: current id " .. id)
 	if id == -1 then id = 0 end
+	
+	local format = modstorage:get_string("timestamp")
+	local time = os.time()
+	message = os.date(format, time) .. message
+
 	modstorage:set_string(id, message)
 	modstorage:set_int("chat_message_id", id + 1)
 end
@@ -50,33 +59,36 @@ local function display_help()
 	minetest.display_chat_message("Off: Turn logging off")
 	minetest.display_chat_message("Clear: Clear chat log")
 	minetest.display_chat_message("Display: Shows logged messages")
+	minetest.display_chat_message("Timestamp: Set the timestamp the mod should use (lua os.date format)")
 	minetest.display_chat_message("Enable_local_log: needed to log messages sent by the local player on servers that don't use recent dev")
 	minetest.display_chat_message("Disable_local_log: needed to prevent double logging of local player messages on servers with recent dev (default)")
 end
 
 minetest.register_chatcommand("chatlog", {
 	func = function(param)
-		param = param:lower()
+		paraml = param:lower()
 
-		if param:sub(1,4) == "help" then
+		if paraml:sub(1,4) == "help" then
 			display_help()
-		elseif param:sub(1,2) == "on" then
+		elseif paraml:sub(1,2) == "on" then
 			modstorage:set_string("disable", "false")
 			return true, "Turned logging on"
-		elseif param:sub(1,3) == "off" then
+		elseif paraml:sub(1,3) == "off" then
 			modstorage:set_string("disable", "true")
 			return true, "Turned logging off"
-		elseif param:sub(1,7) == "display" then
-			return read_log(param:sub(8))
-		elseif param:sub(1,16) == "enable_local_log" then
+		elseif paraml:sub(1,7) == "display" then
+			return read_log(paraml:sub(8))
+		elseif paraml:sub(1,16) == "enable_local_log" then
 			modstorage:set_string("enable_local_log", "true")
 			return true, "Enabled local logging"
-		elseif param:sub(1,17) == "disable_local_log" then
+		elseif paraml:sub(1,17) == "disable_local_log" then
 			modstorage:set_string("enable_local_log", "false")
 			return true, "Disabled local logging"
-		elseif param:sub(1,5) == "clear" then
+		elseif paraml:sub(1,5) == "clear" then
 			modstorage:set_int("chat_message_id", -1)
 			return true, "Cleared log"
+		elseif paraml:sub(1,9) == "timestamp" then
+			modstorage:set_string("timestamp", param:sub(10))
 		else
 			minetest.display_chat_message("Invalid Arguments")
 			display_help()
